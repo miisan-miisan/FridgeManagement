b373b620c0a9b131a8d47e1e47241db9308db7db
田和 瑞樹 <fsi18@opendoor.co.jp>
2022-11-11 19:01:46 +0900

    '登録処理にrequiredを追加'

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -23,7 +23,7 @@
             <label>消費</label>
           </div>
           <label>期限：</label>
-          <input type="date" name="expiration"><br>
+          <input type="date" name="expiration" required><br>
           <label>冷蔵/冷凍</label>
           <div>
             <input type="radio" id="" name="stockType" value=1 checked>
